85e3bc43c1ae033f25f2c35f6c989fa9cdee0f7e
Akshay Nair <phenax5@gmail.com>
2020-06-02 16:57:33 +0530

    Adds test cases for main

---
+++ test/main_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:owyn/main.dart';
+import 'package:owyn/pages/Home.dart';
+import 'package:owyn/pages/Apps.dart';
+
+void main() {
+  testWidgets('Should render with no errors', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+  });
+
+  testWidgets('Should render applications on swipe/fling', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(find.byType(HomeView), findsOneWidget);
+    expect(find.byType(AppsView), findsNothing);
+
+    await tester.fling(find.byType(HomeView), Offset(-100.0, 0.0), 10);
+    await tester.pump();
+
+    expect(find.byType(HomeView), findsOneWidget); // Off screen
+    expect(find.byType(AppsView), findsOneWidget);
+  });
+}
